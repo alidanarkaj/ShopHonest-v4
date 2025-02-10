98ae0ff3d1b2d82039584f152d76b536a7bca4fb
alidanarkaj <alidanark@gmail.com>
2025-02-09 22:17:45 -0500

    dynamic fixes

---
+++ js/clothingItems.js
@@ -0,0 +1,42 @@
+export const clothingItems = [
+  {
+    itemName: "Loose Fit Poplin Shirt",
+    price: 32.0,
+    image: "../assets/1.png",
+  },
+  {
+    itemName: "Pointelle-Knit Polo Shirt",
+    price: 32.0,
+    image: "../assets/2.png",
+  },
+  {
+    itemName: "Cable-Knit Cardigan",
+    price: 28.0,
+    image: "../assets/3.png",
+  },
+  {
+    itemName: "Napped Tie-Belt Jacket",
+    price: 44.0,
+    image: "../assets/4.png",
+  },
+  {
+    itemName: "Fine-Knit Polo Shirt",
+    price: 36.0,
+    image: "../assets/5.png",
+  },
+  {
+    itemName: "Relaxed Fit Tapered Pants",
+    price: 36.0,
+    image: "../assets/6.png",
+  },
+  {
+    itemName: "Sweatshirt with Text Motif",
+    price: 24.0,
+    image: "../assets/7.png",
+  },
+  {
+    itemName: "Button-Detail Ribbed Top",
+    price: 20.0,
+    image: "../assets/8.png",
+  },
+];
---
+++ js/products.js
@@ -0,0 +1,30 @@
+//import the data
+import { clothingItems } from "./clothingItems.js";
+
+//display all items initially from dataset
+window.onload = () => {
+  displayClothes(clothingItems);
+};
+
+//function to display cars
+function displayClothes(clothes) {
+  const itemListings = document.getElementById("itemListings");
+  itemListings.innerHTML = "";
+
+  clothes.forEach((item) => {
+    const itemDiv = document.createElement("div");
+    itemDiv.classList.add("clothing-item");
+
+    itemDiv.innerHTML = `
+      <a href="${productLink}">
+        <img src="${item.image}" alt="${item.itemName}">
+      </a>
+      <div class="description">
+        <p>${item.itemName}</p>
+        <p>$${item.price.toFixed(2)}</p>
+      </div>
+    `;
+
+    itemListings.appendChild(itemDiv);
+  });
+}
